e03c37c426961a0d54fb22c80bb8ff2a538d628c
Akib Bagwan <akibbagwan69@gmail.com>
2024-03-08 20:18:59 +0530

    for testcases check

---
+++ src/searchFunctionality/search.jsx
@@ -0,0 +1,72 @@
+import "./styles.css";
+import axios from "axios";
+import { useState, useEffect } from "react";
+import styles from "./search.module.css"
+
+export default function Search() {
+  const [flags, setFlags] = useState([]);
+  const [change, setChange] = useState(null);
+  console.log(change);
+  useEffect(() => {
+    (async function () {
+      try {
+        let res = await axios.get("https://restcountries.com/v3.1/all");
+        // console.log(res.data);
+        setFlags(res.data);
+      } catch (error) {
+        console.log(error);
+      }
+    })();
+  }, []);
+
+  useEffect(() => {
+    (async function () {
+      try {
+        if (change !== null) {
+          let res = await axios.get(
+            `https://restcountries.com/v3.1/name/${change}`
+          );
+          setFlags([]);
+          setFlags(res.data);
+        }
+      } catch (error) {
+        if (error.response.status === 404) {
+          setFlags([]);
+          setChange(null);
+        }
+        console.log(error);
+      }
+      // finally {
+      //   setChange(null);
+      // }
+    })();
+  }, [change]);
+  return (
+    <div className={styles.App}>
+      <div className={styles.searchParent}>
+        <input
+          onChange={(e) => {
+            if (e.target.value) {
+              setChange(e.target.value);
+            } else {
+              setChange(null);
+            }
+          }}
+          className={styles.search}
+          type="text"
+          placeholder="Search for Countries..."
+        />
+      </div>
+      <div className={styles.flagsParent}>
+        {flags.length > 0 &&
+          flags.map((flag) => (
+            <div className={styles.countryCard} key={flag.cca3}>
+              <img className={styles.flagImg} src={flag.flags.png} alt="" />
+              <p>{flag.name.common}</p>
+            </div>
+          ))}
+        {flags.length === 0 && ""}
+      </div>
+    </div>
+  );
+}
