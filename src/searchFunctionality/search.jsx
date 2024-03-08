3ad8c64f27a54090c0fa98a5846b03f015c65e06
Akib Bagwan <akibbagwan69@gmail.com>
2024-03-08 21:37:46 +0530

    for checking testcases

--- src/searchFunctionality/search.jsx
+++ src/searchFunctionality/search.jsx
@@ -1,71 +1,136 @@
-import axios from "axios";
-import { useState, useEffect } from "react";
-import styles from "./search.module.css"
+// import axios from "axios";
+// import { useState, useEffect } from "react";
+// import styles from "./search.module.css"
 
-export default function Search() {
-  const [flags, setFlags] = useState([]);
-  const [change, setChange] = useState(null);
-  console.log(change);
+// export default function Search() {
+//   const [flags, setFlags] = useState([]);
+//   const [change, setChange] = useState(null);
+//   console.log(change);
+//   useEffect(() => {
+//     (async function () {
+//       try {
+//         let res = await axios.get("https://restcountries.com/v3.1/all");
+//         // console.log(res.data);
+//         setFlags(res.data);
+//       } catch (error) {
+//         console.log(error);
+//       }
+//     })();
+//   }, []);
+
+//   useEffect(() => {
+//     (async function () {
+//       try {
+//         if (change !== null) {
+//           let res = await axios.get(
+//             `https://restcountries.com/v3.1/name/${change}`
+//           );
+//           setFlags([]);
+//           setFlags(res.data);
+//         }
+//       } catch (error) {
+//         if (error.response.status === 404) {
+//           setFlags([]);
+//           setChange(null);
+//         }
+//         console.log(error);
+//       }
+//       // finally {
+//       //   setChange(null);
+//       // }
+//     })();
+//   }, [change]);
+//   return (
+//     <div className={styles.App}>
+//       <div className={styles.searchParent}>
+//         <input
+//           onChange={(e) => {
+//             if (e.target.value) {
+//               setChange(e.target.value);
+//             } else {
+//               setChange(null);
+//             }
+//           }}
+//           className={styles.search}
+//           type="text"
+//           placeholder="Search for Countries..."
+//         />
+//       </div>
+//       <div className={styles.countryCard}>
+//         {flags.length > 0 &&
+//           flags.map((flag) => (
+//             <div className={styles.flagChild} key={flag.cca3}>
+//               <img className={styles.flagImg} src={flag.flags.png} alt="" />
+//               <p>{flag.name.common}</p>
+//             </div>
+//           ))}
+//       </div>
+//     </div>
+//   );
+// }
+import "./style.css";
+import { useEffect, useState } from "react";
+
+function App() {
+  const [countries, setCountries] = useState([]);
+  const [filtered, setFiltered] = useState([]);
+  const [search, setSearch] = useState("");
+
+  const handleChange = (e) => {
+    setSearch(e.target.value);
+  };
+
   useEffect(() => {
-    (async function () {
+    const fetchData = async () => {
       try {
-        let res = await axios.get("https://restcountries.com/v3.1/all");
-        // console.log(res.data);
-        setFlags(res.data);
-      } catch (error) {
-        console.log(error);
+        const resp = await fetch("https://restcountries.com/v3.1/all");
+        const data = await resp.json();
+        setCountries(data);
+      } catch (err) {
+        console.log(err);
       }
-    })();
+    };
+    fetchData();
   }, []);
 
   useEffect(() => {
-    (async function () {
-      try {
-        if (change !== null) {
-          let res = await axios.get(
-            `https://restcountries.com/v3.1/name/${change}`
-          );
-          setFlags([]);
-          setFlags(res.data);
-        }
-      } catch (error) {
-        if (error.response.status === 404) {
-          setFlags([]);
-          setChange(null);
-        }
-        console.log(error);
-      }
-      // finally {
-      //   setChange(null);
-      // }
-    })();
-  }, [change]);
+    const data = countries.filter((country) =>
+      country.name.common.toLowerCase().includes(search.toLowerCase())
+    );
+    setFiltered(data);
+  }, [search]);
+
+  console.log(countries);
   return (
-    <div className={styles.App}>
-      <div className={styles.searchParent}>
+    <div>
+      <div className="inp">
         <input
-          onChange={(e) => {
-            if (e.target.value) {
-              setChange(e.target.value);
-            } else {
-              setChange(null);
-            }
-          }}
-          className={styles.search}
           type="text"
-          placeholder="Search for Countries..."
+          placeholder="Enter a country"
+          onChange={(e) => handleChange(e)}
         />
       </div>
-      <div className={styles.countryCard}>
-        {flags.length > 0 &&
-          flags.map((flag) => (
-            <div className={styles.flagChild} key={flag.cca3}>
-              <img className={styles.flagImg} src={flag.flags.png} alt="" />
-              <p>{flag.name.common}</p>
-            </div>
-          ))}
-        {flags.length === 0 && ""}
+      <div className="App">
+        {search === ""
+          ? countries.map((country) => {
+              return (
+                <div className="countryCard">
+                  <img src={country.flags.png} alt={country.flag}></img>
+                  <h3>{country.name.common}</h3>
+                </div>
+              );
+            })
+          : filtered.map((country) => {
+              return (
+                <div className="countryCard">
+                  <img src={country.flags.png} alt={country.flag}></img>
+                  <h3>{country.name.common}</h3>
+                </div>
+              );
+            })}
       </div>
     </div>
   );
 }
+
+export default App;
