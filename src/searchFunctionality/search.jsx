fbf5540fd2e86ae05cdc3795af96aef35cb57eb6
Akib Bagwan <akibbagwan69@gmail.com>
2024-03-09 12:49:17 +0530

    h3 to h2

--- src/searchFunctionality/search.jsx
+++ src/searchFunctionality/search.jsx
@@ -124,7 +124,7 @@ export default function Search() {
               return (
                 <div className="countryCard">
                   <img src={country.flags.png} alt={country.flag}></img>
-                  <h3>{country.name.common}</h3>
+                  <h2>{country.name.common}</h2>
                 </div>
               );
             })}
